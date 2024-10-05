9d581906590f4d875d68daa84f4b2e613cdd38b2
Valkywar1 <lopezpaulina415@gmail.com>
2024-10-05 02:06:03 -0600

    Solución de instalación de Jetstream y solución con archivos para migrar

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,13 @@ Route::get('/', function () {
 Route::resource('users', UserController::class);
 
 Route::resource('products', ProductController::class);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
