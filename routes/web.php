f9b8238aaa214c3bbd3fcf9419a8fa3843f48911
Luis Fernando <luis.alcala4615@alumnos.udg.mx>
2024-10-05 23:42:50 -0600

    Se agrego plantilla para registro de usuarios y se arreglaron unos problemitas extra

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Auth\LoginController;
-
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\ProductController;
 
 
 Route::get('/', function () {
@@ -17,3 +18,6 @@ Route::resource('products', ProductController::class);
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
