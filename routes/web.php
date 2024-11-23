605a2e43d45e88de1f073d411955798366514f9a
Valkywar1 <lopezpaulina415@gmail.com>
2024-11-23 12:10:38 -0600

    pruebas v.2

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Rutas de autenticación (login, register, logout)
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
+// // Rutas de autenticación (login, register, logout)
+// Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+// Route::post('/login', [LoginController::class, 'login']);
+// Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+// Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+// Route::post('/register', [RegisterController::class, 'register']);
 
 // Proteger las rutas con el middleware 'auth'
 Route::middleware(['auth'])->group(function () {
